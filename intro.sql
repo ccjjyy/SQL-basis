29d2cd65ec89838331c14bd72e76e9ae91ad1f38
ccjjyy <153336897@qq.com>
2017-06-22 10:02:03 +0800

    Update intro.sql

--- intro.sql
+++ intro.sql
@@ -44,6 +44,47 @@ SELECT title
 FROM films
 WHERE release_year > 1994 AND < 2000;
 
+SELECT *
+FROM films
+WHERE language = 'Spanish' 
+AND release_year > 2000 
+AND release_year < 2010;
+
+# OR
+SELECT title, release_year
+FROM films
+WHERE (release_year >= 1990 AND release_year <= 1999)
+AND (language = 'French' OR language = 'Spanish')
+AND (gross > 2000000);
+
+# BETWEEN
+SELECT  title, release_year
+FROM films
+WHERE (release_year BETWEEN 1990 AND 2000)
+AND (budget > 100000000)
+AND (language = 'Spanish' OR language = 'French');
+
+# IN
+SELECT title, language
+FROM films
+WHERE language IN ('English', 'Spanish', 'French');
+
+# IS NULL 
+# IS NOT NULL
+SELECT COUNT(*)
+FROM films
+WHERE language IS NULL;
+
+# LIKE
+# NOT LIKE
+# % _
+SELECT name
+FROM people
+WHERE name LIKE '_r%';
+
+
+
+
 
 
 
